773b2a14bfb407250e7f45ab8d4041690d65aae5
Akhil <akhil.potukuchi@gmail.com>
2022-05-19 17:06:21 +0530

    Delete aliases during user deletion

--- lib/Listeners/UserDeletedListener.php
+++ lib/Listeners/UserDeletedListener.php
@@ -5,26 +5,28 @@ declare(strict_types=1);
 namespace OCA\EcloudAccounts\Listeners;
 
 use Curl;
+use Exception;
 use OCA\EcloudAccounts\AppInfo\Application;
 use OCP\EventDispatcher\Event;
 use OCP\EventDispatcher\IEventListener;
 use OCP\IConfig;
 use OCP\ILogger;
+use OCP\IUserManager;
 use OCP\User\Events\UserDeletedEvent;
 
 require_once 'curl.class.php';
 
 class UserDeletedListener implements IEventListener
 {
-
     private $logger;
     private $config;
 
-    public function __construct(ILogger $logger, IConfig $config)
+    public function __construct(ILogger $logger, IConfig $config, IUserManager $userManager)
     {
         $this->logger = $logger;
         $this->config = $config;
     }
+
 
     public function handle(Event $event): void
     {
@@ -32,7 +34,18 @@ class UserDeletedListener implements IEventListener
             return;
         }
 
-        $uid = $event->getUser()->getUID();
+        $user = $event->getUser();
+        $email = $user->getEMailAddress();
+        $uid = $user->getUID();
+
+        try {
+            $conn = $this->getLDAPConnection($uid);
+            $this->deleteAliasEntries($conn, $email);
+        }
+        catch (Exception $e) {
+            $this->logger->error('Error deleting aliases for user '. $uid . ' :' . $e->getMessage());
+        }
+
         $this->logger->info("PostDelete user {user}", array('user' => $uid));
         $this->ecloudDelete(
             $uid,
@@ -53,7 +66,6 @@ class UserDeletedListener implements IEventListener
      */
     public function ecloudDelete(string $userID, string $welcomeDomain, string $welcomeSecret)
     {
-
         $postDeleteUrl = "https://" . $welcomeDomain . "/postDelete.php";
         $curl = new Curl();
 
@@ -62,7 +74,6 @@ class UserDeletedListener implements IEventListener
          * Handling the non NC part of deletion process
          */
         try {
-
             $headers = array(
                 'Content-Type: application/json'
             );
@@ -80,5 +91,71 @@ class UserDeletedListener implements IEventListener
         }
 
         return null;
+    }
+
+    private function deleteAliasEntries($conn, string $email)
+    {
+        $aliasBaseDn = getenv('LDAP_ALIASES_BASE_DN');
+        $aliasDns = $this->getAliasEntries($conn, $aliasBaseDn, $email);
+        foreach($aliasDns as $aliasDn) {
+            $deleted = ldap_delete($conn, $aliasDn);
+            if(!$deleted) {
+                $this->logger->error('Deleting alias ' . $aliasDn . ' for email ' .  $email . ' failed');
+            }
+        }
+    }
+
+    private function getAliasEntries($conn, string $aliasBaseDn, string $email) : array
+    {
+        $filter = "(mailAddress=$email)";
+        $aliasEntries = ldap_search($conn, $aliasBaseDn, $filter);
+        if (!$aliasEntries) {
+            return [];
+        }
+
+        $aliasEntries =  ldap_get_entries($conn, $aliasEntries);
+        $aliasEntries = array_filter($aliasEntries, fn($entry) => is_array($entry));
+        $aliasEntries = array_map(
+            fn ($entry) => $entry['dn'],
+            $aliasEntries
+        );
+
+        return $aliasEntries;
+    }
+
+    private function getLDAPConnection(string $uid)
+    {
+        $backends = $this->userManager->getBackends();
+        $conn = null;
+        foreach ($backends as $backend) {
+            if ($backend->getBackendName() === 'LDAP') {
+                $access = $backend->getLDAPAccess($uid);
+                $connection = $access->getConnection();
+                $configuration = $connection->getConfiguration();
+
+                if (!$configuration->ldapConfigurationActive) {
+                    continue;
+                }
+
+                $adminDn = $configuration->ldapAgentName;
+                $adminPassword = $configuration->ldapAgentPassword;
+                $host = $configuration->ldapHost;
+                $port = $configuration->ldapPort;
+
+                $conn = ldap_connect($host, $port);
+                ldap_set_option($conn, LDAP_OPT_PROTOCOL_VERSION, 3);
+                ldap_bind($conn, $adminDn, $adminPassword);
+
+                if (!$conn) {
+                    continue;
+                }
+
+                return $conn;
+            }
+        }
+
+        if (!$conn) {
+            throw new Exception('Could not connect to LDAP server to delete aliases for user ' . $uid);
+        }
     }
 }
