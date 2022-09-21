a26656980cfd64596b4f152a134536e039a13d00
Akhil <akhil.potukuchi@gmail.com>
2022-09-21 16:08:59 +0530

    correct webpack

--- webpack.js
+++ webpack.js
@@ -4,7 +4,6 @@ const path = require('path')
 module.exports = {
 	...webpackConfig,
 	entry: {
-		'admin-settings': path.join(__dirname, 'src/admin.js'),
 		'personal-settings': path.join(__dirname, 'src/personal.js'),
 	},
 }
