db1d94b47cdf624cb5d6170e1fc3d03a92a5e90f
Akhil <akhil.potukuchi@gmail.com>
2022-09-21 16:06:46 +0530

    correct app id

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -41,7 +41,7 @@ use OCA\EcloudAccounts\Listeners\UserChangedListener;
 class Application extends App implements IBootstrap
 {
 
-    const APP_ID = 'drop_account';
+    const APP_ID = 'ecloud-accounts';
 
     public function __construct(array $urlParams = [])
     {
