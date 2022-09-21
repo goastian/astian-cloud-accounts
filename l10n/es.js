bf6f102f9e876c1a68095fd5ad7db5c4c23e9b91
Ronak Patel <ronak.patel@e.email>
2022-09-21 19:25:26 +0530

    translation files

--- l10n/es.js
+++ l10n/es.js
@@ -1,8 +1,8 @@
-{
-	"translations": {
+OC.L10N.register(
+    "drop_account",
+    {
 		"Delete shop account": "Delete shop account",
 		"You can delete your shop account with deleting ecloud account.": "You can delete your shop account with deleting ecloud account.",
 		"Check this to confirm the deletion request for shop account": "Check this to confirm the deletion request for shop account"
-	},
-	"pluralForm": "nplurals=2; plural=(n > 1);"
-}
+},
+"nplurals=2; plural=(n != 1);");
--- l10n/it.js
+++ l10n/it.js
@@ -1,8 +1,8 @@
 OC.L10N.register(
-	"drop_account",
-	{
+    "drop_account",
+    {
 		"Delete shop account": "Delete shop account",
 		"You can delete your shop account with deleting ecloud account.": "You can delete your shop account with deleting ecloud account.",
 		"Check this to confirm the deletion request for shop account": "Check this to confirm the deletion request for shop account"
-	},
-	"nplurals=2; plural=(n > 1);");
+},
+"nplurals=2; plural=(n != 1);");
