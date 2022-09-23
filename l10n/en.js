3ae751f614cd1beafa482b99520b210c546a3a7b
Nivesh Krishna <nivesh@e.email>
2022-09-23 09:33:16 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- l10n/en.js
+++ l10n/en.js
@@ -2,7 +2,7 @@ OC.L10N.register(
     "drop_account",
     {
 		"Delete shop account": "Delete shop account",
-		"You can delete your shop account with deleting ecloud account.": "You can delete your shop account with deleting ecloud account.",
+		"You can delete your shop account with deleting cloud account.": "You can delete your shop account with deleting cloud account.",
 		"Check this to confirm the deletion request for shop account": "Check this to confirm the deletion request for shop account"
 },
 "nplurals=2; plural=(n != 1);");
