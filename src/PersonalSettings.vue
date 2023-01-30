76f8f702b85b460d704908de663d4874cd3c5cb4
Ronak Patel <ronak.patel@enrysisland.com>
2023-01-31 00:20:01 +0530

    reverted code

--- src/PersonalSettings.vue
+++ src/PersonalSettings.vue
@@ -160,7 +160,7 @@ export default {
 		async getOrdersInfo() {
 			try {
 				const url = generateUrl(
-					`/apps/${this.appName}/shop-accounts/order_info?userId=11758`
+					`/apps/${this.appName}/shop-accounts/order_info?userId=${this.shopUser.id}`
 				)
 				const { status, data } = await Axios.get(url)
 				if (status === 200) {
