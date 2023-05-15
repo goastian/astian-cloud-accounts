ce2ede9c31f4e4cecc7da649447e9f9902c96fb8
Avinash Gusain <avinashg@e.email>
2023-05-15 12:30:01 +0530

    code fix

--- lib/Migration/CreateTasksCalendar.php
+++ lib/Migration/CreateTasksCalendar.php
@@ -83,14 +83,14 @@ class CreateTasksCalendar implements IRepairStep {
 	 */
 	public function getPrincipalUriByCalendar():array {
 		$query = $this->connection->getQueryBuilder();
-        $query= $expr = $qb->expr();
+		$query = $expr = $qb->expr();
 		$query->select(['Distinct c1.principaluri'])
 			->from('calendars', 'c1')
-			->leftJoin('c1', 'calendars', 'c2',$expr->andX(
-                    $expr->eq('c1.principaluri', 'c2.principaluri'),
-                    $expr->eq('c2.uri', $qb->createNamedParameter('tasks')),
-                    $expr->eq('c2.components', $qb->createNamedParameter('VTODO'))
-                )
+			->leftJoin('c1', 'calendars', 'c2', $expr->andX(
+				$expr->eq('c1.principaluri', 'c2.principaluri'),
+				$expr->eq('c2.uri', $qb->createNamedParameter('tasks')),
+				$expr->eq('c2.components', $qb->createNamedParameter('VTODO'))
+			)
 			)
 			->where($query->expr()->isNull('c2.principaluri'));
 		$stmt = $query->executeQuery();
