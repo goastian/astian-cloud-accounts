9376bfd819936eaf9ade3c456e88f1d3b77f4b72
Ronak Patel <ronak.patel@e.email>
2023-08-28 02:06:11 -0700

    change in account service

--- lib/Service/AccountService.php
+++ lib/Service/AccountService.php
@@ -5,25 +5,59 @@ declare(strict_types=1);
 namespace OCA\EcloudAccounts\Service;
 
 use OCP\IConfig;
-use OCP\IGroupManager;
-use OCP\IUserSession;
-use OCP\App\IAppManager;
+use Exception;
 
 class AccountService {
 	private $config;
-	private $appManager;
-	protected $groupManager;
-	private $userSession;
+	private $LDAPConnectionService;
 
 	public function __construct(
 		IConfig $config,
-		IGroupManager $groupManager,
-		IUserSession $userSession,
-		IAppManager $appManager
+		LDAPConnectionService $LDAPConnectionService,
 	) {
 		$this->config = $config;
-		$this->groupManager = $groupManager;
-		$this->userSession = $userSession;
-		$this->appManager = $appManager;
+		$this->LDAPConnectionService = $LDAPConnectionService;
+	}
+	public function registerUser(string $displayname, string $email, string $username, string $password) {
+		$connection = $this->LDAPConnectionService->getLDAPConnection();
+		$base = $this->LDAPConnectionService->getLDAPBaseUsers()[0];
+
+		// Check if the username already exists
+		$filter = "(usernameWithoutDomain=$username)";
+		$searchResult = ldap_search($connection, $base, $filter);
+
+		if (!$searchResult) {
+			throw new Exception("Error while searching Murena username.");
+		}
+
+		$entries = ldap_get_entries($connection, $searchResult);
+		$domain = $this->config->getSystemValue('main_domain', '');
+		if ($entries['count'] > 0) {
+			return false;
+		} else {
+			$newUserDN = "username=$username@$domain," . $base;
+			$userClusterID = 'HEL01';
+			$newUserEntry = [
+				'mailAddress' => $username . '@' . $domain,
+				'username' => $username . '@' . $domain,
+				'usernameWithoutDomain' => $username,
+				'userPassword' => $password,
+				'displayName' => $displayname,
+				'quota' => $this->LDAPConnectionService->getLdapQuota(),
+				'mailAlternate' => '',
+				'recoveryMailAddress' => $email,
+				'active' => 'TRUE',
+				'mailActive' => 'TRUE',
+				'userClusterID' => $userClusterID,
+				'objectClass' => ['murenaUser', 'simpleSecurityObject']
+			];
+
+			$ret = ldap_add($connection, $newUserDN, $newUserEntry);
+
+			if (!$ret) {
+				throw new Exception("Error while creating Murena account.");
+			}
+			return true;
+		}
 	}
 }
