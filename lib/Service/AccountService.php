04a379ae3b328d9186408c700d8903889b203fd5
Ronak Patel <ronak.patel@e.email>
2023-08-28 06:36:51 -0700

    code refactoring

--- lib/Service/AccountService.php
+++ lib/Service/AccountService.php
@@ -21,43 +21,43 @@ class AccountService {
 	public function registerUser(string $displayname, string $email, string $username, string $password) {
 		$connection = $this->LDAPConnectionService->getLDAPConnection();
 		$base = $this->LDAPConnectionService->getLDAPBaseUsers()[0];
-
+	
 		// Check if the username already exists
 		$filter = "(usernameWithoutDomain=$username)";
 		$searchResult = ldap_search($connection, $base, $filter);
-
+	
 		if (!$searchResult) {
 			throw new Exception("Error while searching Murena username.");
 		}
-
+	
 		$entries = ldap_get_entries($connection, $searchResult);
-		$domain = $this->config->getSystemValue('main_domain', '');
 		if ($entries['count'] > 0) {
 			return false;
-		} else {
-			$newUserDN = "username=$username@$domain," . $base;
-			$userClusterID = 'HEL01';
-			$newUserEntry = [
-				'mailAddress' => $username . '@' . $domain,
-				'username' => $username . '@' . $domain,
-				'usernameWithoutDomain' => $username,
-				'userPassword' => $password,
-				'displayName' => $displayname,
-				'quota' => $this->LDAPConnectionService->getLdapQuota(),
-				'mailAlternate' => '',
-				'recoveryMailAddress' => $email,
-				'active' => 'TRUE',
-				'mailActive' => 'TRUE',
-				'userClusterID' => $userClusterID,
-				'objectClass' => ['murenaUser', 'simpleSecurityObject']
-			];
-
-			$ret = ldap_add($connection, $newUserDN, $newUserEntry);
-
-			if (!$ret) {
-				throw new Exception("Error while creating Murena account.");
-			}
-			return true;
 		}
+		
+		$domain = $this->config->getSystemValue('main_domain', '');
+		$newUserDN = "username=$username@$domain," . $base;
+		$userClusterID = 'HEL01';
+		$newUserEntry = [
+			'mailAddress' => "$username@$domain",
+			'username' => "$username@$domain",
+			'usernameWithoutDomain' => $username,
+			'userPassword' => $password,
+			'displayName' => $displayname,
+			'quota' => $this->LDAPConnectionService->getLdapQuota(),
+			'recoveryMailAddress' => $email,
+			'active' => 'TRUE',
+			'mailActive' => 'TRUE',
+			'userClusterID' => $userClusterID,
+			'objectClass' => ['murenaUser', 'simpleSecurityObject']
+		];
+	
+		$ret = ldap_add($connection, $newUserDN, $newUserEntry);
+	
+		if (!$ret) {
+			throw new Exception("Error while creating Murena account.");
+		}
+	
+		return true;
 	}
 }
