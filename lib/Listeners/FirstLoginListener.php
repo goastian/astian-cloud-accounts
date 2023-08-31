d3f4a3f92ebbf91b875d897e7e6154db327563dd
Ronak Patel <ronak.patel@e.email>
2023-08-31 09:04:34 -0700

    added static

--- lib/Listeners/FirstLoginListener.php
+++ lib/Listeners/FirstLoginListener.php
@@ -9,16 +9,18 @@ use OCP\EventDispatcher\IEventListener;
 use OCP\ILogger;
 use OCP\Mail\IMailer;
 use OCP\IConfig;
-use OCP\Util;
+use OCA\EcloudAccounts\Service\UserService;
 
 class FirstLoginListener implements IEventListener {
 	private $logger;
 	private $mailer;
 	private $config;
-	public function __construct(ILogger $logger, IMailer $mailer, IConfig $config) {
+	private $userService;
+	public function __construct(ILogger $logger, IMailer $mailer, IConfig $config, UserService $userService) {
 		$this->logger = $logger;
 		$this->mailer = $mailer;
 		$this->config = $config;
+		$this->userService = $userService;
 	}
 
 	public function handle(Event $event): void {
@@ -31,31 +33,10 @@ class FirstLoginListener implements IEventListener {
 	 * @param string $username
 	 * @return bool
 	 */
-	public static function sendWelcomeEmail(string $displayname, string $username) {
-		$title = 'Welcome to Murena Email Service!';
-		$description = 'Dear '.$displayname.',\nWe are thrilled to welcome you to Murena Email Service! It\'s a pleasure to have you on board and we are excited about the journey ahead.
-		\nAt Murena, we are committed to providing you with a seamless and secure email experience. Our user-friendly interface, advanced features, and robust security measures have been designed to ensure that your communication remains efficient, effective, and protected.
-		\nAs you explore our platform, you will discover a range of features tailored to meet your email needs. From easy-to-use organization tools to powerful search capabilities, we aim to enhance your productivity and streamline your communication.
-		\nYour privacy and security are of utmost importance to us. Rest assured that we employ state-of-the-art encryption and multi-layered security protocols to safeguard your sensitive information.
-		\nShould you require any assistance or have any questions, our dedicated support team is here to help. Don\'t hesitate to reach out at [support email] for any inquiries or concerns.
-		\nOnce again, welcome to the Murena Email Service community! We\'re excited to have you on board and look forward to serving your email needs.
-		\nBest regards,
-		\nMurena Team';
-		$domain = $this->config->getSystemValue('main_domain', '');
-		$fromEmail = $username.'@'.$domain;
-
-		$template = $this->mailer->createEMailTemplate('account.SendWelcomeEmail', []);
-		$template->addHeader();
-		$template->setSubject($title);
-		$template->addBodyText(htmlspecialchars($description), $description);
-
-		$message = $this->mailer->createMessage();
-		$message->setFrom([Util::getDefaultEmailAddress('noreply')]);
-		$message->setReplyTo([$fromEmail => $displayname]);
-		$message->setTo([$fromEmail]);
-		$message->useTemplate($template);
-
-		$this->mailer->send($message);
+	public static function sendWelcomeEmail() {
+		$displayname = 'Ronak Patel';
+		$username = 'rptest46';
+		$this->userService->sendWelcomeEmail($displayname, $username);
 		return true;
 	}
 }
