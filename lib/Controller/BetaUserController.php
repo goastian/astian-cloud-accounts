b938dab542495620ff73792cb3ed8b4cbad34644
Akhil <akhil@e.email>
2023-09-14 13:12:50 +0530

    Run lint

--- lib/Controller/BetaUserController.php
+++ lib/Controller/BetaUserController.php
@@ -7,10 +7,10 @@
 namespace OCA\EcloudAccounts\Controller;
 
 use OCP\AppFramework\Controller;
-use OCP\IRequest;
 use OCP\IConfig;
-use OCP\IUserManager;
 use OCP\IGroupManager;
+use OCP\IRequest;
+use OCP\IUserManager;
 use OCP\IUserSession;
 use OCP\Mail\IMailer;
 use OCP\Util;
