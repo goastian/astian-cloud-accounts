b9ef767dc1aab5a010bb810f138e7f7c06528502
Ronak Patel <ronak.patel@e.email>
2023-10-17 02:15:20 -0700

    change in oc

--- src/Signup.vue
+++ src/Signup.vue
@@ -283,7 +283,7 @@ export default {
 			return rotationVariations[Math.floor(Math.random() * rotationVariations.length)]
 		},
 		onLanguageChange() {
-			this.$i18n.global.locale = this.selectedLanguage
+			this.$i18n.locale = this.selectedLanguage
 		},
 
 	},
