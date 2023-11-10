db685af957c5382fe1a2ce3072dbb34167e3d87e
Ronak Patel <ronak.patel@e.email>
2023-11-10 10:58:30 -0800

    check username

--- src/signup/RegistrationForm.vue
+++ src/signup/RegistrationForm.vue
@@ -275,7 +275,7 @@ export default {
 
 		async checkUsername() {
 			const data = {
-				username: this.formData.username + '@' + this.domain,
+				username: this.formData.username,
 			}
 			const url = generateUrl(`/apps/${this.appName}/accounts/check_username_available`)
 			try {
