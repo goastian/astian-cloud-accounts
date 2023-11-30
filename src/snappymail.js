688801baf0a8c59890b4dca599b9e43c9fb7a767
Avinash Gusain <avinashg@e.email>
2023-11-30 16:05:39 +0530

    fix indent

--- src/snappymail.js
+++ src/snappymail.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-	let targetElement = document.getElementById("rl-app")
-    let newDiv = document.createElement("div")
-    newDiv.textContent = "Please verify your recovery address"
+	const targetElement = document.getElementById('rl-app')
+    const newDiv = document.createElement('div')
+    newDiv.textContent = 'Please verify your recovery address'
     targetElement.insertBefore(newDiv, targetElement.firstChild)
 })
