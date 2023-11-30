2af1a86dc701cd9c13a36417e650af4ceae28b37
Avinash Gusain <avinashg@e.email>
2023-11-30 16:03:26 +0530

    check event fired

--- src/snappymail.js
+++ src/snappymail.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-	var targetElement = document.getElementById("rl-app");
-    var newDiv = document.createElement("div");
-    newDiv.textContent = "Please verify your recovery address";
-    targetElement.insertBefore(newDiv, targetElement.firstChild);
+	let targetElement = document.getElementById("rl-app")
+    let newDiv = document.createElement("div")
+    newDiv.textContent = "Please verify your recovery address"
+    targetElement.insertBefore(newDiv, targetElement.firstChild)
 })
