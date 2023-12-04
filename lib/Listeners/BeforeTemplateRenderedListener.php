c733e2cda0146284961064cbc3a97dc211173ce3
Avinash Gusain <avinashg@e.email>
2023-12-04 20:22:39 +0530

    recovery email link

--- lib/Listeners/BeforeTemplateRenderedListener.php
+++ lib/Listeners/BeforeTemplateRenderedListener.php
@@ -14,76 +14,87 @@ use OCP\ISession;
 use OCP\IUserSession;
 use OCP\Util;
 
-class BeforeTemplateRenderedListener implements IEventListener {
-	private $userSession;
-	private $request;
-	private $appName;
-	private $session;
-	private $config;
-	private $appManager;
-	private Util $util;
+class BeforeTemplateRenderedListener implements IEventListener
+{
+    private $userSession;
+    private $request;
+    private $appName;
+    private $session;
+    private $config;
+    private $appManager;
+    private Util $util;
 
-	private const SNAPPYMAIL_APP_ID = 'snappymail';
-	private const SNAPPYMAIL_URL = '/apps/snappymail/';
-	private const SNAPPYMAIL_AUTOLOGIN_PWD = '1';
+    private const SNAPPYMAIL_APP_ID = 'snappymail';
+    private const SNAPPYMAIL_URL = '/apps/snappymail/';
+    private const SNAPPYMAIL_AUTOLOGIN_PWD = '1';
 
-	public function __construct($appName, IUserSession $userSession, IRequest $request, ISession $session, IConfig $config, IAppManager $appManager, Util $util) {
-		$this->appName = $appName;
-		$this->userSession = $userSession;
-		$this->request = $request;
-		$this->session = $session;
-		$this->config = $config;
-		$this->appManager = $appManager;
-		$this->util = $util;
-	}
+    public function __construct($appName, IUserSession $userSession, IRequest $request, ISession $session, IConfig $config, IAppManager $appManager, Util $util)
+    {
+        $this->appName = $appName;
+        $this->userSession = $userSession;
+        $this->request = $request;
+        $this->session = $session;
+        $this->config = $config;
+        $this->appManager = $appManager;
+        $this->util = $util;
+    }
 
-	public function handle(Event $event): void {
-		if (!($event instanceof BeforeTemplateRenderedEvent)) {
-			return;
-		}
-		if ($this->userSession->isLoggedIn() && $this->appManager->isEnabledForUser(self::SNAPPYMAIL_APP_ID) && strpos($this->request->getPathInfo(), self::SNAPPYMAIL_URL) !== false) {
-			$this->autoLoginWebmail();
-		}
-		$pathInfo = $this->request->getPathInfo();
-		$username = $this->userSession->getUser()->getUID();
-		$emailVerified = $this->config->getUserValue($username, 'email-recovery', 'recovery-email-verified', 'true');
-		if($emailVerified == 'false') {
-			if (strpos($pathInfo, '/apps/snappymail/') !== false) {
-				$this->util->addScript($this->appName, $this->appName .'-snappymail');
-			}
-		}
-		
-	}
+    public function handle(Event $event): void
+    {
+        if (!($event instanceof BeforeTemplateRenderedEvent)) {
+            return;
+        }
+        if ($this->userSession->isLoggedIn() && $this->appManager->isEnabledForUser(self::SNAPPYMAIL_APP_ID) && strpos($this->request->getPathInfo(), self::SNAPPYMAIL_URL) !== false) {
+            $this->autoLoginWebmail();
+        }
+       
+        $user = $this->userSession->getUser();
+        if ($user instanceof IUser) {
+            $username = $currentUser->getUID();
+			$pathInfo = $this->request->getPathInfo();
+            $emailVerified = $this->config->getUserValue($username, 'email-recovery', 'recovery-email-verified', 'true');
+            if($emailVerified == 'false') {
+                if (strpos($pathInfo, '/apps/snappymail/') !== false) {
+                    $this->util->addScript($this->appName, $this->appName . '-snappymail');
+                }
+            }
+        }
 
 
-	private function autoLoginWebmail() {
-		$isOidcLogin = $this->session->get('is_oidc');
-		if (!$isOidcLogin) {
-			return;
-		}
-		$accountId = $this->getAccountId();
-		$actions = \RainLoop\Api::Actions();
 
-		if (empty($accountId) || $actions->getMainAccountFromToken(false)) {
-			return;
-		}
+    }
 
-		// Just send over '1' as password to trigger login as the plugin will set the correct access token
-		$password = self::SNAPPYMAIL_AUTOLOGIN_PWD; // As we cannot pass by reference to LoginProcess
-		$account = $actions->LoginProcess($accountId, $password, false);
-		if ($account) {
-			$actions->Plugins()->RunHook('login.success', array($account));
-			$actions->SetAuthToken($account);
-		}
-	}
 
-	private function getAccountId(): string {
-		$username = $this->userSession->getUser()->getUID();
-		if ($this->config->getAppValue('snappymail', 'snappymail-autologin', false)) {
-			return $username;
-		}
-		if ($this->config->getAppValue('snappymail', 'snappymail-autologin-with-email', false)) {
-			return $this->config->getUserValue($username, 'settings', 'email', '');
-		}
-	}
+    private function autoLoginWebmail()
+    {
+        $isOidcLogin = $this->session->get('is_oidc');
+        if (!$isOidcLogin) {
+            return;
+        }
+        $accountId = $this->getAccountId();
+        $actions = \RainLoop\Api::Actions();
+
+        if (empty($accountId) || $actions->getMainAccountFromToken(false)) {
+            return;
+        }
+
+        // Just send over '1' as password to trigger login as the plugin will set the correct access token
+        $password = self::SNAPPYMAIL_AUTOLOGIN_PWD; // As we cannot pass by reference to LoginProcess
+        $account = $actions->LoginProcess($accountId, $password, false);
+        if ($account) {
+            $actions->Plugins()->RunHook('login.success', array($account));
+            $actions->SetAuthToken($account);
+        }
+    }
+
+    private function getAccountId(): string
+    {
+        $username = $this->userSession->getUser()->getUID();
+        if ($this->config->getAppValue('snappymail', 'snappymail-autologin', false)) {
+            return $username;
+        }
+        if ($this->config->getAppValue('snappymail', 'snappymail-autologin-with-email', false)) {
+            return $this->config->getUserValue($username, 'settings', 'email', '');
+        }
+    }
 }
