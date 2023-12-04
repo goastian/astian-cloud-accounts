fde6d9993d939fb4861356ca2e8a750ade33d84e
Avinash Gusain <avinashg@e.email>
2023-12-04 19:15:01 +0530

    recovery email link

--- src/snappymail.js
+++ src/snappymail.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
 	const targetElement = document.getElementById('rl-app')
 	const newDiv = document.createElement('div')
-	newDiv.textContent = 'Please verify your recovery address'
+	newDiv.className = 
+	newDiv.textContent = 'Please set your recovery email address now and use your email account without restrictions.'
+	const link = document.createElement('a')
+	link.textContent = 'SET RECOVERY EMAIL NOW' 
+	link.href = 'settings/user/security'
+	link.style.display = 'block'
+	newDiv.appendChild(link)
 	targetElement.insertBefore(newDiv, targetElement.firstChild)
 })
