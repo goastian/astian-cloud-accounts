6a3c07837d5ec569d82d0bf6169e0a880f707232
Avinash Gusain <avinashg@e.email>
2023-12-08 12:48:51 +0530

    routing

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -26,6 +26,7 @@ return ['routes' => [
 	],
 	
 	['name' => 'account#index', 'url' => '/accounts/{lang}/signup', 'verb' => 'GET'],
+	['name' => 'account#index_without_lang', 'url' => '/accounts/signup', 'verb' => 'GET'],
 	['name' => 'account#create', 'url' => '/accounts/create', 'verb' => 'POST'],
 	['name' => 'account#captcha', 'url' => '/accounts/captcha', 'verb' => 'GET'],
 	['name' => 'account#verify_captcha', 'url' => '/accounts/verify_captcha', 'verb' => 'POST'],
--- lib/Controller/AccountController.php
+++ lib/Controller/AccountController.php
@@ -74,6 +74,25 @@ class AccountController extends Controller {
 	 * @PublicPage
 	 * @NoCSRFRequired
 	 *
+	 * @param string $lang Language code (default: 'en')
+	 *
+	 */
+	public function index_without_lang(string $lang = 'en') {
+		if ($this->userSession->isLoggedIn()) {
+			return new RedirectResponse($this->urlGenerator->linkToDefaultPageUrl());
+		}
+		return new TemplateResponse(
+			Application::APP_ID,
+			'signup',
+			['appName' => Application::APP_ID, 'lang' => $lang],
+			TemplateResponse::RENDER_AS_GUEST
+		);
+	}
+	/**
+	 * @NoAdminRequired
+	 * @PublicPage
+	 * @NoCSRFRequired
+	 *
 	 * @param string $displayname      User's display name
 	 * @param string $recoveryEmail    User's recovery email
 	 * @param string $username         User's username
