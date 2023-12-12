6c3e46a94e4868857f15e879fbb2ae9e6abeec54
Avinash Gusain <avinashg@e.email>
2023-12-12 16:41:34 +0530

    recovery email banner

--- src/emailrecovery.js
+++ src/emailrecovery.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
 	const newDiv = document.createElement('div')
 	newDiv.className = 'recovery-email'
 	const img = document.createElement('img')
-    img.src = OC.generateUrl('/custom_apps/' + APPLICATION_NAME + '/img/warning.svg')
-    newDiv.appendChild(img);
+	img.src = OC.generateUrl('/custom_apps/' + APPLICATION_NAME + '/img/warning.svg')
+	newDiv.appendChild(img)
 	newDiv.textContent = t('ecloud-accounts', 'Please set your recovery email address now and use your email account without restrictions.')
 	const link = document.createElement('a')
 	link.textContent = t('ecloud-accounts', 'SET RECOVERY EMAIL NOW')
