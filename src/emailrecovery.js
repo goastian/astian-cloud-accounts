ac13cf3db9ef686bea9fe5488ef578c35390642f
Avinash Gusain <avinashg@e.email>
2023-12-12 16:31:44 +0530

    recovery email banner

--- src/emailrecovery.js
+++ src/emailrecovery.js
@@ -1,8 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 	const targetElement = document.getElementById('header')
 	const newDiv = document.createElement('div')
 	newDiv.className = 'recovery-email'
-	newDiv.appendChild('<svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 21 20" fill="none"><path d="M1.3335 17.9167H19.6668L10.5002 2.08334L1.3335 17.9167ZM11.3335 15.4167H9.66683V13.75H11.3335V15.4167ZM11.3335 12.0833H9.66683V8.75001H11.3335V12.0833Z" fill="#FFBB00"/></svg>')
+	const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+	svg.setAttribute('width', '21')
+	svg.setAttribute('height', '20')
+	svg.setAttribute('viewBox', '0 0 21 20')
+	svg.setAttribute('fill', 'none')
+	const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+	path.setAttribute('d', 'M1.3335 17.9167H19.6668L10.5002 2.08334L1.3335 17.9167ZM11.3335 15.4167H9.66683V13.75H11.3335V15.4167ZM11.3335 12.0833H9.66683V8.75001H11.3335V12.0833Z')
+	path.setAttribute('fill', '#FFBB00')
+	svg.appendChild(path)
+	newDiv.prepend(svg)
 	newDiv.textContent = t('ecloud-accounts', 'Please set your recovery email address now and use your email account without restrictions.')
 	const link = document.createElement('a')
 	link.textContent = t('ecloud-accounts', 'SET RECOVERY EMAIL NOW')
