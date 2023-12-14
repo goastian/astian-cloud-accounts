7f1ee85398d7aad8273521ce16797378e340a270
Avinash Gusain <avinashg@e.email>
2023-12-14 20:09:51 +0530

    logo alignment fix

--- src/signup/RegistrationForm.vue
+++ src/signup/RegistrationForm.vue
@@ -325,7 +325,7 @@ export default {
 	float: right;
 	position: absolute;
 	top: 54px;
-	right: 0;
+	right: 150px;
 }
 .fields input[type='checkbox'].checkbox + label:before{
 	height: 15px;
@@ -527,6 +527,18 @@ p.validation-success:before {
 		font-size: 1.0em;
 		line-height: 1.0em;
 	}
+	.lang-select {
+		font-size: 14px;
+		line-height:24px;
+		color: rgba(0, 0, 0, 0.6);
+		font-weight:400;
+		background: url("../../img/expand_more.svg") no-repeat right 8px center;
+		float: right;
+		position: absolute;
+		top: 54px;
+		right: 10px;
+	}
+	
 }
 @media screen and (max-width: 500px) {
 	#main {
