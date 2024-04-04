21396180d984b635d536b2717a012b8502d73ae6
Ronak Patel <ronak.patel@e.email>
2024-04-04 19:14:18 +0000

    better error handling

--- lib/Controller/AccountController.php
+++ lib/Controller/AccountController.php
@@ -169,7 +169,8 @@ class AccountController extends Controller {
 			$response->setData(['message' => 'A server-side error occurred while processing your request! Please try again later.', 'success' => false]);
 			$response->setStatus(500);
 		} catch (Exception $e) {
-			$response->setData(['message' => $e->getMessage(), 'success' => false]);
+			$this->logger->logException($e, ['app' => Application::APP_ID]);
+			$response->setData(['message' => 'An error occurred while creating your account!', 'success' => false]);
 			$response->setStatus(500);
 		}
 
