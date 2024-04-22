e27014519f435d82094c1cb554dcac3b0cf00839
Akhil <akhil@e.email>
2024-04-22 17:37:28 +0530

    Fix namespace of SSOService

--- lib/Command/Migrate2FASecrets.php
+++ lib/Command/Migrate2FASecrets.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace OCA\EcloudAccounts\Command;
 
-use OCA\EcloudAccounts\Db\SSOService;
+use OCA\EcloudAccounts\Service\SSOService;
 use OCA\EcloudAccounts\Db\TwoFactorMapper;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
