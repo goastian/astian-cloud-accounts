815821fe34932f263952f026716d8de542a1f8ea
Avinash Gusain <avinashg@e.email>
2024-05-13 21:49:17 +0530

    by pass token para

--- src/signup/CaptchaForm.vue
+++ src/signup/CaptchaForm.vue
@@ -92,8 +92,12 @@ export default {
 				const data = {
 					captchaInput: this.formData.captchaInput,
 				}
+				/* eslint-disable */
+				console.log(this.bypassToken)
 				if (this.bypassToken === 1) {
+					console.log("here")
 					data.bypassToken = this.bypassToken
+					console.log("done")
 				}
 				const url = generateUrl(`/apps/${this.appName}/accounts/verify_captcha`)
 				await Axios.post(url, data)
