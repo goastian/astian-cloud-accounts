0d945457dbd64ba64f587ea17b10a2b1067c26cf
Ronak Patel <ronak.patel@e.email>
2024-05-15 23:40:20 -0700

    added command #13

---
+++ lib/Command/SyncBlacklistedDomains.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\EcloudAccounts\Command;
+
+use OCA\EcloudAccounts\Db\BlacklistedDomainMapper;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SyncBlacklistedDomains extends Command {
+	private OutputInterface $commandOutput;
+	private BlacklistedDomainMapper $blacklistedDomainMapper;
+
+	public function __construct(BlacklistedDomainMapper $blacklistedDomainMapper) {
+		$this->blacklistedDomainMapper = $blacklistedDomainMapper;
+		parent::__construct();
+	}
+
+	protected function configure(): void {
+		$this
+			->setName('ecloud-accounts:migrate-blacklisted-domains')
+			->setDescription('Migrates blacklisted domains')
+			->addOption(
+				'users',
+				null,
+				InputOption::VALUE_OPTIONAL,
+				'comma separated list of users',
+				''
+			);
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output): int {
+		$this->blacklistedDomainMapper->updateBlacklistedDomains();
+	}
+}
