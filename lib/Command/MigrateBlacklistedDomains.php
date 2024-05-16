2a2d28e4127976c9c8bd1c5a1d68e6abc92804db
Ronak Patel <ronak.patel@e.email>
2024-05-15 23:11:29 -0700

    added command #9

--- lib/Command/MigrateBlacklistedDomains.php
+++ lib/Command/MigrateBlacklistedDomains.php
@@ -4,24 +4,28 @@ declare(strict_types=1);
 
 namespace OCA\EcloudAccounts\Command;
 
-use OCA\EcloudAccounts\Db\BlacklistedDomainMapper;
+use OCA\EcloudAccounts\Db\TwoFactorMapper;
+use OCA\EcloudAccounts\Service\SSOService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class MigrateBlacklistedDomains extends Command {
+	private SSOService $ssoService;
+	private TwoFactorMapper $twoFactorMapper;
 	private OutputInterface $commandOutput;
-	private BlacklistedDomainMapper $blacklistedDomainMapper;
 
-	public function __construct(BlacklistedDomainMapper $blacklistedDomainMapper) {
-		$this->blacklistedDomainMapper = $blacklistedDomainMapper;
+	public function __construct(SSOService $ssoService, TwoFactorMapper $twoFactorMapper) {
+		$this->ssoService = $ssoService;
+		$this->twoFactorMapper = $twoFactorMapper;
 		parent::__construct();
 	}
 
 	protected function configure(): void {
 		$this
-			->setName('ecloud-accounts:migrate-blacklisted-domains')
-			->setDescription('Migrates blacklisted domains')
+			->setName('ecloud-accounts:migrate-2fa-secrets')
+			->setDescription('Migrates 2FA secrets to SSO database')
 			->addOption(
 				'users',
 				null,
@@ -32,6 +36,35 @@ class MigrateBlacklistedDomains extends Command {
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output): int {
-		$this->blacklistedDomainMapper->updateBlacklistedDomains();
+		try {
+			$this->commandOutput = $output;
+			$usernames = [];
+			$usernameList = $input->getOption('users');
+			if (!empty($usernameList)) {
+				$usernames = explode(',', $usernameList);
+			}
+			$this->migrateUsers($usernames);
+			return 0;
+		} catch (\Exception $e) {
+			$this->commandOutput->writeln($e->getMessage());
+			return 1;
+		}
+	}
+
+	/**
+	 * Migrate user secrets to the SSO database
+	 *
+	 * @return void
+	 */
+	private function migrateUsers(array $usernames = []) : void {
+		$entries = $this->twoFactorMapper->getEntries($usernames);
+		foreach ($entries as $entry) {
+			try {
+				$this->ssoService->migrateCredential($entry['username'], $entry['secret']);
+			} catch (\Exception $e) {
+				$this->commandOutput->writeln('Error inserting entry for user ' . $entry['username'] . ' message: ' . $e->getMessage());
+				continue;
+			}
+		}
 	}
 }
