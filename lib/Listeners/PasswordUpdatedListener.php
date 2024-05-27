9c6e5bc4e8f86bcda295ffc4486953d45cda3bfe
Fahim Salam Chowdhury <fahim@e.email>
2024-05-27 18:51:40 +0600

    fix: php lint

--- lib/Listeners/PasswordUpdatedListener.php
+++ lib/Listeners/PasswordUpdatedListener.php
@@ -4,25 +4,19 @@ declare(strict_types=1);
 
 namespace OCA\EcloudAccounts\Listeners;
 
-use Exception;
 use OCA\EcloudAccounts\Service\SSOService;
 use OCP\EventDispatcher\Event;
 use OCP\EventDispatcher\IEventListener;
-use OCP\ILogger;
 use OCP\User\Events\PasswordUpdatedEvent;
 
 class PasswordUpdatedListener implements IEventListener {
+	private SSOService $ssoService;
 
-    private SSOService $ssoService;
-
-    private $logger;
-
-    public function __construct(ILogger $logger, SSOService $ssoService) {
-		$this->logger = $logger;
+	public function __construct(SSOService $ssoService) {
 		$this->ssoService = $ssoService;
 	}
 
-    public function handle(Event $event): void {
+	public function handle(Event $event): void {
 		if (!($event instanceof PasswordUpdatedEvent)) {
 			return;
 		}
@@ -30,10 +24,6 @@ class PasswordUpdatedListener implements IEventListener {
 		$user = $event->getUser();
 		$username = $user->getUID();
 
-        try {
-            $this->ssoService->logout($username);
-        } catch (Exception $e) {
-            $this->logger->logException('Failed to logout user from OIDC onPasswordChange: ' . $username, ['exception' => $e]);
-        }
+		$this->ssoService->logout($username);
 	}
 }
