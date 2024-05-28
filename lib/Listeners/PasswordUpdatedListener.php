2d19748c87448c854959b038bd5b43c9d5bba185
Fahim Salam Chowdhury <fahim@e.email>
2024-05-28 15:47:50 +0600

    chore: add try-catch block on ssoLogout on passwordChange

--- lib/Listeners/PasswordUpdatedListener.php
+++ lib/Listeners/PasswordUpdatedListener.php
@@ -4,16 +4,22 @@ declare(strict_types=1);
 
 namespace OCA\EcloudAccounts\Listeners;
 
+use Exception;
 use OCA\EcloudAccounts\Service\SSOService;
 use OCP\EventDispatcher\Event;
 use OCP\EventDispatcher\IEventListener;
+use OCP\ILogger;
 use OCP\User\Events\PasswordUpdatedEvent;
 
 class PasswordUpdatedListener implements IEventListener {
+	
 	private SSOService $ssoService;
 
-	public function __construct(SSOService $ssoService) {
+	private $logger;
+
+	public function __construct(SSOService $ssoService, ILogger $logger) {
 		$this->ssoService = $ssoService;
+		$this->logger = $logger;
 	}
 
 	public function handle(Event $event): void {
@@ -24,6 +30,11 @@ class PasswordUpdatedListener implements IEventListener {
 		$user = $event->getUser();
 		$username = $user->getUID();
 
-		$this->ssoService->logout($username);
+		try {
+			$this->ssoService->logout($username);
+		} catch (Exception $e) {
+			$this->logger->logException('Failed to logout from ssoService for user: ' . $username, ['exception' => $e]);
+		}
 	}
 }
+
