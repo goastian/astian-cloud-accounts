55c0fabcd37bf34b24fe3e3d5d91c687b2ef0e88
Fahim Salam Chowdhury <fahim@e.email>
2024-06-21 19:13:06 +0600

    feat: check userLoggedIn or not at passwordUpdateListener

--- lib/Listeners/PasswordUpdatedListener.php
+++ lib/Listeners/PasswordUpdatedListener.php
@@ -11,6 +11,7 @@ use OCP\EventDispatcher\Event;
 use OCP\EventDispatcher\IEventListener;
 use OCP\ILogger;
 use OCP\ISession;
+use OCP\IUserSession;
 use OCP\User\Events\PasswordUpdatedEvent;
 
 class PasswordUpdatedListener implements IEventListener {
@@ -19,11 +20,13 @@ class PasswordUpdatedListener implements IEventListener {
 
 	private ILogger $logger;
 	private ISession $session;
+	private IUserSession $userSession;
 
-	public function __construct(SSOService $ssoService, ILogger $logger, ISession $session) {
+	public function __construct(SSOService $ssoService, ILogger $logger, ISession $session, IUserSession $userSession) {
 		$this->ssoService = $ssoService;
 		$this->logger = $logger;
 		$this->session = $session;
+		$this->userSession = $userSession;
 	}
 
 	public function handle(Event $event): void {
@@ -31,14 +34,14 @@ class PasswordUpdatedListener implements IEventListener {
 			return;
 		}
 
+		if (!$this->userSession->isLoggedIn() || !$this->session->exists('is_oidc')) {
+			return;
+		}
+
 		$user = $event->getUser();
 		$username = $user->getUID();
 
 		try {
-			if (!$this->session->exists('is_oidc')) {
-				return;
-			}
-
 			$this->ssoService->logout($username);
 		} catch (Exception $e) {
 			$this->logger->logException($e, ['app' => Application::APP_ID]);
