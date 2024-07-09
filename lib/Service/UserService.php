f3171f010df561f9f0de432a0b91768a07ed5b27
Ronak Patel <ronak.patel@e.email>
2024-07-09 10:34:57 +0530

    changes

--- lib/Service/UserService.php
+++ lib/Service/UserService.php
@@ -256,7 +256,7 @@ class UserService {
 			throw new Exception("Username '$username' is already taken.");
 		}
 		if (!empty($recoveryEmail)) {
-			$this->recoveryEmailService->validateRecoveryEmail($recoveryEmail);
+			$this->recoveryEmailService->validateRecoveryEmail('', $recoveryEmail);
 		}
 		$this->addNewUserToLDAP($displayname, $username, $userEmail, $password);
 	}
