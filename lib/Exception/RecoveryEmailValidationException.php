ae338232282635be3a67a7e53968c83a61ab77c2
Ronak Patel <ronak.patel@e.email>
2024-07-09 09:28:48 +0530

    added exception

--- lib/Exception/RecoveryEmailValidationException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace OCA\EcloudAccounts\Exception;
-
-class RecoveryEmailValidationException extends \Exception {
-	public function __construct($message = null, $code = 0) {
-		parent::__construct($message, $code);
-	}
-}
