3a8cc1aa66da49757d9bf0e984af08312e292068
Ronak Patel <ronak.patel@e.email>
2024-07-10 13:41:22 +0530

    new changes added

--- lib/Listeners/BeforeUserCreatedListener.php
+++ lib/Listeners/BeforeUserCreatedListener.php
@@ -7,6 +7,7 @@ namespace OCA\EcloudAccounts\Listeners;
 use InvalidArgumentException;
 use OCP\EventDispatcher\Event;
 use OCP\EventDispatcher\IEventListener;
+use OCP\ILogger;
 use OCP\User\Events\BeforeUserCreatedEvent;
 use OCP\User\Events\CreateUserEvent;
 
@@ -16,23 +17,26 @@ use OCP\User\Events\CreateUserEvent;
  * @package OCA\EcloudAccounts\EventListener\User
  */
 class BeforeUserCreatedListener implements IEventListener {
-
-	public function __construct() {
+	private $logger;
+	public function __construct(ILogger $logger) {
+		$this->logger = $logger;
 	}
 
 	/**
 	 * @param Event $event
 	 */
 	public function handle(Event $event): void {
+		$this->logger->info('=====> INSIDE BeforeUserCreatedListener');
 		if($event instanceof BeforeUserCreatedEvent || $event instanceof CreateUserEvent) {
-			$this->preventCreationIfDeleted($event->getUid());
+			$this->preventRecoveryEmailifNotValid($event->getUid());
 		}
 	}
 
 	/**
 	 * @param string $userId
 	 */
-	protected function preventCreationIfDeleted(string $userId): void {
+	protected function preventRecoveryEmailifNotValid(string $userId): void {
+		$this->logger->info('=====> INSIDE preventRecoveryEmailifNotValid');
 		throw new InvalidArgumentException("The username {$userId} is queued for deletion");
 	}
 }
