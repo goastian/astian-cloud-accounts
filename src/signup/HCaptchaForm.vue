9d0b911cc4c31e8e92cfae7f9b82ca5e7623d7ea
Akhil <akhil.potukuchi@gmail.com>
2024-07-24 23:01:39 +0530

    Fix component declaration for hcaptcha

--- src/signup/HCaptchaForm.vue
+++ src/signup/HCaptchaForm.vue
@@ -1,15 +1,19 @@
 <template>
-	<vue-hcaptcha :sitekey="siteKey" @verify="onVerify" />
+	<VueHcaptcha :sitekey="siteKey" @verify="onVerify" />
 </template>
 
 <script>
 import { loadState } from '@nextcloud/initial-state'
 import Axios from '@nextcloud/axios'
 import { generateUrl } from '@nextcloud/router'
+import { VueHcaptcha } from '@hcaptcha/vue-hcaptcha'
 
 const APPLICATION_NAME = 'ecloud-accounts'
 
 export default {
+	components: {
+		VueHcaptcha,
+	},
 	data() {
 		return {
 			siteKey: loadState(APPLICATION_NAME, 'hCaptchaSiteKey'),
