b249525d7189c39dca057c236e1304d64fd83516
theronakpatel <eronax59@gmail.com>
2024-07-24 14:35:47 +0530

    changes

--- src/signup/RegistrationForm.vue
+++ src/signup/RegistrationForm.vue
@@ -212,12 +212,12 @@ export default {
 		},
 	},
 	created() {
-		const currentURL = window.location.href;
-		const urlSegments = currentURL.split('/');
-		const languages = ['de', 'fr', 'it', 'es', 'en'];
-		const potentialLang = urlSegments[urlSegments.length - 2];
+		const currentURL = window.location.href
+		const urlSegments = currentURL.split('/')
+		const languages = ['de', 'fr', 'it', 'es', 'en']
+		const potentialLang = urlSegments[urlSegments.length - 2]
 
-		this.formData.selectedLanguage = languages.includes(potentialLang) ? potentialLang : 'en';
+		this.formData.selectedLanguage = languages.includes(potentialLang) ? potentialLang : 'en'
 	},
 	methods: {
 		validateForm(fieldsToValidate) {
