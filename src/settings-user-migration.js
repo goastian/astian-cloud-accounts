35fc99627752fa267d4a458f7c75d4ec7769797d
Avinash Gusain <avinashg@e.email>
2024-08-30 00:10:51 +0530

    lint fix

--- src/settings-user-migration.js
+++ src/settings-user-migration.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Select the #google-content element
-    const googleContent = document.getElementById('google-content')
+	// Select the #google-content element
+	const googleContent = document.getElementById('google-content')
 
-    // Create a new p element
-    const newParagraph = document.createElement('p')
-    newParagraph.textContent =  t('⚠️ If you see a "Google hasn\'t verified this app" message you can bypass it by clicking "Advanced". We\'re currently working on passing the certification Google demands to get rid of this message.')
+	// Create a new p element
+	const newParagraph = document.createElement('p')
+	newParagraph.textContent = t('⚠️ If you see a "Google hasn\'t verified this app" message you can bypass it by clicking "Advanced". We\'re currently working on passing the certification Google demands to get rid of this message.')
 
-    // Insert the p element before the h3 element
-    googleContent.insertBefore(newParagraph, googleContent.querySelector('h3'))
+	// Insert the p element before the h3 element
+	googleContent.insertBefore(newParagraph, googleContent.querySelector('h3'))
 
 })
