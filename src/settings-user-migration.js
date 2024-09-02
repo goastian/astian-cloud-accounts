c691b774af5ab28d518ff40437e602b797978b22
Avinash Gusain <avinashg@e.email>
2024-09-03 00:13:03 +0530

    added log

--- src/settings-user-migration.js
+++ src/settings-user-migration.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const googleContent = document.getElementById('google-content')
+	const googleContent = document.getElementById('google-content')
 
-    const translationKey = "If you see a \"Google hasn't verified this app\" message you can bypass it by clicking \"Advanced\". We're currently working on passing the certification Google demands to get rid of this message.";
-    const translatedText = t(translationKey)
-    
-    // Debugging to see if the translation is correct
-    console.log('Translated text:', translatedText)
-    
-    const newParagraph = document.createElement('p')
-    newParagraph.textContent = '⚠️ ' + translatedText;
+	const translationKey = "If you see a \"Google hasn't verified this app\" message you can bypass it by clicking \"Advanced\". We're currently working on passing the certification Google demands to get rid of this message."
+	const translatedText = t(translationKey)
 
-    googleContent.insertBefore(newParagraph, googleContent.querySelector('h3'))
+	// eslint-disable-next-line no-console
+	console.log('Translated text:', translatedText)
+
+	const newParagraph = document.createElement('p')
+	newParagraph.textContent = '⚠️ ' + translatedText
+
+	googleContent.insertBefore(newParagraph, googleContent.querySelector('h3'))
 })
