3dcad60fe656a9ba48772d15ef4c51a5e48ff689
Avinash Gusain <avinashg@e.email>
2024-09-03 18:08:53 +0530

    svg added

--- src/settings-user-migration.js
+++ src/settings-user-migration.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	const translatedText = t('ecloud-accounts', translationKey)
 
 	const newParagraph = document.createElement('p')
-	newParagraph.textContent = '⚠️ ' + translatedText
+	newParagraph.textContent = '<svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="warning"><path id="Vector" d="M1.334 17.917h18.333L10.5 2.083 1.334 17.917zM11.334 15.417H9.667v-1.667h1.667v1.667zm0-3.334H9.667V8.75h1.667v3.333z" fill="#FFBB00"/></g></svg>' + translatedText
 
 	googleContent.insertBefore(newParagraph, googleContent.querySelector('h3'))
 })
