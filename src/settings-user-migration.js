4da32f56219d10eb1efd0ae2d1083b9596eea99d
Avinash Gusain <avinashg@e.email>
2024-09-03 11:21:22 +0530

    remove console log

--- src/settings-user-migration.js
+++ src/settings-user-migration.js
@@ -4,9 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
 	const translationKey = "If you see a \"Google hasn't verified this app\" message you can bypass it by clicking \"Advanced\". We're currently working on passing the certification Google demands to get rid of this message."
 	const translatedText = t('ecloud-accounts', translationKey)
 
-	// eslint-disable-next-line no-console
-	console.log('Translated text:', translatedText)
-
 	const newParagraph = document.createElement('p')
 	newParagraph.textContent = '⚠️ ' + translatedText
 
