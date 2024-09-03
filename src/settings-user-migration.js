f50ee30b9d7671bc416cba264db66d0a40faa217
Avinash Gusain <avinashg@e.email>
2024-09-03 18:17:10 +0530

    svg added

--- src/settings-user-migration.js
+++ src/settings-user-migration.js
@@ -3,9 +3,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	const translationKey = "If you see a \"Google hasn't verified this app\" message you can bypass it by clicking \"Advanced\". We're currently working on passing the certification Google demands to get rid of this message."
 	const translatedText = t('ecloud-accounts', translationKey)
+	const img = createImageElement('email-recovery')
 
 	const newParagraph = document.createElement('p')
-	newParagraph.textContent = '<svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="warning"><path id="Vector" d="M1.334 17.917h18.333L10.5 2.083 1.334 17.917zM11.334 15.417H9.667v-1.667h1.667v1.667zm0-3.334H9.667V8.75h1.667v3.333z" fill="#FFBB00"/></g></svg>' + translatedText
+	// Add the img element to the paragraph
+	newParagraph.appendChild(img);
+
+	newParagraph.textContent = ' ' + translatedText
 
 	googleContent.insertBefore(newParagraph, googleContent.querySelector('h3'))
 })
+
+
+/**
+ *
+ * @param appName
+ */
+function createImageElement(appName) {
+	const img = document.createElement('img')
+	img.src = generateUrl('/custom_apps/' + appName + '/img/warning.svg')
+	return img
+}
+
