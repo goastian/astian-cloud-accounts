e1ee8f1975e13eb1191b0456254779555caa891e
Avinash Gusain <avinashg@e.email>
2024-09-03 11:13:53 +0530

    added appname

--- src/settings-user-migration.js
+++ src/settings-user-migration.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	const googleContent = document.getElementById('google-content')
 
 	const translationKey = "If you see a \"Google hasn't verified this app\" message you can bypass it by clicking \"Advanced\". We're currently working on passing the certification Google demands to get rid of this message."
-	const translatedText = t(translationKey)
+	const translatedText = t('ecloud-accounts', translationKey)
 
 	// eslint-disable-next-line no-console
 	console.log('Translated text:', translatedText)
