e1f9b103615a423c18a6866f85543550bfcd07cb
Avinash Gusain <avinashg@e.email>
2024-09-03 18:37:51 +0530

    removed svg

--- src/settings-user-migration.js
+++ src/settings-user-migration.js
@@ -1,31 +1,11 @@
-import { generateUrl } from '@nextcloud/router'
-
 document.addEventListener('DOMContentLoaded', function() {
 	const googleContent = document.getElementById('google-content')
 
 	const translationKey = "If you see a \"Google hasn't verified this app\" message you can bypass it by clicking \"Advanced\". We're currently working on passing the certification Google demands to get rid of this message."
 	const translatedText = t('ecloud-accounts', translationKey)
-	const img = createImageElement('email-recovery')
 
 	const newParagraph = document.createElement('p')
-	// Add the img element to the paragraph
-	newParagraph.appendChild(img)
-
-	const textNode = document.createTextNode(' ' + translatedText)
-	newParagraph.appendChild(textNode)
+	newParagraph.textContent = '⚠️ ' + translatedText
 
 	googleContent.insertBefore(newParagraph, googleContent.querySelector('h3'))
 })
-
-/**
- *
- * @param appName
- * @return {HTMLImageElement}
- */
-function createImageElement(appName) {
-	const img = document.createElement('img')
-	img.src = generateUrl('/custom_apps/' + appName + '/img/warning.svg')
-	img.alt = 'Warning'
-	img.style.verticalAlign = 'middle'
-	return img
-}
