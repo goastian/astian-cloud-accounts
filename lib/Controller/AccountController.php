882c9075969afa74392c7c18d4ff7ddf11e3b18f
theronakpatel <eronax59@gmail.com>
2024-10-31 01:11:36 +0530

    temp

--- lib/Controller/AccountController.php
+++ lib/Controller/AccountController.php
@@ -261,7 +261,7 @@ class AccountController extends Controller {
 	 *
 	 */
 	public function tempApiCheck(string $username) {
-		echo 'test';		
+		echo 'test';
 	}
 	/**
 	 * Check if a username is available.
