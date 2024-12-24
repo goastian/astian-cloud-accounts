a5a212bdda46882ae9d78a2c5ce5a1880ee300b3
theronakpatel <eronax59@gmail.com>
2024-12-24 10:54:54 +0530

    rate limit removed for GET method

--- lib/Command/DeleteSendgridContact.php
+++ lib/Command/DeleteSendgridContact.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class DeleteSendgridContactCommand extends Command {
+class DeleteSendgridContact extends Command {
 	private SendGridService $sendGridService;
 
 	public function __construct(SendGridService $sendGridService) {
